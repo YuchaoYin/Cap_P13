140667ae32667b03db075115a99708713b9be09e
YuchaoYin <yinyuchao1990@hotmail.com>
2019-01-27 04:52:03 -0800

    add libraries needed

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -4,6 +4,9 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 from scipy.spatial import KDTree
+import numpy as np
+import rospy
+from std_msgs.msg import Int32
 
 import math
 

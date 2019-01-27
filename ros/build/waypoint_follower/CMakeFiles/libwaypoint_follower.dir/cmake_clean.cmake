5a550d41afda5afc00727c7f003035057023a9e4
YuchaoYin <yinyuchao1990@hotmail.com>
2019-01-26 20:27:54 -0800

    waypoint updater partial

---
+++ ros/build/waypoint_follower/CMakeFiles/libwaypoint_follower.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libwaypoint_follower.dir/lib/libwaypoint_follower.cpp.o"
+  "/home/student/Documents/P13/ros/devel/lib/liblibwaypoint_follower.pdb"
+  "/home/student/Documents/P13/ros/devel/lib/liblibwaypoint_follower.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libwaypoint_follower.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
